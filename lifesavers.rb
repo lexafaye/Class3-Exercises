b315ec7f6babaa252b353479f14db90c1c4813c1
Alexandra Edelstein <alexandra.edelstein@gmail.com>
2015-05-26 20:27:09 -0400

    lifesavers

---
+++ lifesavers.rb
@@ -0,0 +1,6 @@
+lifesavers = ["red", "green", "green", "yellow", "red"]
+puts lifesavers
+lifesavers[1] = "red"
+lifesavers[3] = "green"
+lifesavers[4] = "yellow"
+puts lifesavers
---
+++ lifesaversII.rb
@@ -0,0 +1,5 @@
+lifesavers = ["red", "green", "green", "yellow", "red"]
+puts lifesavers
+lifesavers.unshift "red"
+lifesavers.pop
+puts lifesavers
